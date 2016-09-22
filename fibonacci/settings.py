48125868eda33e2e2284fe28886fa04e999165fb
sami <mdsamiul05@gmail.com>
2016-09-23 03:21:44 +0530

    add

--- fibonacci/settings.py
+++ fibonacci/settings.py
@@ -97,7 +97,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-print str(os.path.join(BASE_DIR,'static'))
+# print str(os.path.join(BASE_DIR,'static'))
 
 STATICFILES_DIRS = (
     STATIC_PATH,
