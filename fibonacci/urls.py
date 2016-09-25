1d0dabded35ed0a36744ec70ade04a89d3f96e08
samiforgit <mdsamiul05@gmail.com>
2016-09-25 13:35:36 +0530

    chnages in urlpatters

--- fibonacci/urls.py
+++ fibonacci/urls.py
@@ -1,14 +1,6 @@
-from django.conf.urls import patterns, include, url
-import sys
-from django.contrib import admin
+from django.conf.urls import url
 from fibonacci import views
 
-urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', views.fibonacci_home , name='fibonacci_home'),
-    # url(r'^blog/', include('blog.urls')),
-    # url(r'^admin/', include(admin.site.urls)),
-    # url(r'^fibonacci-series/(?P<nterms>[A-Za-z0-9+/=]+)/$', views.fibonacci , name='fibonacci'),
-
-    url(r'^fibonacci/$',views.fibonacci, name='fibonacci'),
-)
+urlpatterns = [
+    url(r'^$',views.fibonacci, name='fibonacci'),
+]
